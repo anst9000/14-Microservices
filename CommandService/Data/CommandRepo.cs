7d57dfa892b1d26dc4ead00918546dc956507b29
Anders Strömberg <coltlaboy@yahoo.se>
2023-11-28 15:34:03 +0100

    Created RabbitMQ message bus

--- CommandService/Data/CommandRepo.cs
+++ CommandService/Data/CommandRepo.cs
@@ -1,43 +1,67 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using CommandService.Models;
 
 namespace CommandService.Data
 {
     public class CommandRepo : ICommandRepo
     {
+        private readonly AppDbContext _context;
+
+        public CommandRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public void CreateCommand(int platformId, Command command)
         {
-            throw new System.NotImplementedException();
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            command.PlatformId = platformId;
+            _context.Commands.Add(command);
         }
 
         public void CreatePlatform(Platform platform)
         {
-            throw new System.NotImplementedException();
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            _context.Platforms.Add(platform);
         }
 
         public IEnumerable<Platform> GetAllPlatforms()
         {
-            throw new System.NotImplementedException();
+            return _context.Platforms.ToList();
         }
 
         public Command GetCommand(int platformId, int commandId)
         {
-            throw new System.NotImplementedException();
+            return _context.Commands
+                        .Where(c => c.PlatformId == platformId && c.Id == commandId)
+                        .FirstOrDefault();
         }
 
         public IEnumerable<Command> GetCommandsForPlatform(int platformId)
         {
-            throw new System.NotImplementedException();
+            return _context.Commands
+                .Where(c => c.PlatformId == platformId)
+                .OrderBy(c => c.Platform.Name);
         }
 
         public bool PlatformExists(int platformId)
         {
-            throw new System.NotImplementedException();
+            return _context.Platforms.Any(p => p.Id == platformId);
         }
 
         public bool SaveChanges()
         {
-            throw new System.NotImplementedException();
+            return _context.SaveChanges() >= 0;
         }
     }
 }
---
+++ PlatformService/AsyncDataServices/MessageBusClient.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using PlatformService.Dtos;
+using RabbitMQ.Client;
+
+namespace PlatformService.AsyncDataServices
+{
+    public class MessageBusClient : IMessageBusClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public MessageBusClient(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            // Setup a connection factory
+            var factory = new ConnectionFactory()
+            {
+                HostName = _configuration["RabbitMQHost"],
+                Port = int.Parse(_configuration["RabbitMQPort"])
+            };
+
+            try
+            {
+                _connection = factory.CreateConnection();
+                _channel = _connection.CreateModel();
+
+                _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+
+                _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+
+                Console.WriteLine("--> Connected to RabbitMQ MessageBus");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Could not connect to RabbitMQ Message Bus: {ex.Message}");
+            }
+        }
+
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            Console.WriteLine("--> RabbitMQ Connection Shutdown");
+        }
+
+        public void PublishNewPlatform(PlatformPublishedDto platformPublishedDto)
+        {
+            var message = JsonSerializer.Serialize(platformPublishedDto);
+
+            if (_connection.IsOpen)
+            {
+                Console.WriteLine("--> RabbitMQ Connection Open, sending message");
+
+                SendMessage(message);
+            }
+            else
+            {
+                Console.WriteLine("--> RabbitMQ Connection is closed, NOT sending");
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("--> RabbitMQ MessageBus Disposed");
+
+            if (_channel.IsOpen)
+            {
+                _channel.Close();
+                _connection.Close();
+            }
+        }
+
+        private void SendMessage(string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+
+            _channel.BasicPublish(
+                exchange: "trigger",
+                routingKey: "",
+                basicProperties: null,
+                body: body
+            );
+
+            Console.WriteLine($"--> We have sent a message: {message}");
+        }
+    }
+}
